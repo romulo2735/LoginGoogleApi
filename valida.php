d9f4dd49627c10efe92c9d13b1c1430bf5dd6f90
Romulo Sousa <romulosousa27@hotmail.com>
2017-10-10 22:29:02 -0300

    Criando ao conexão entre o banco e a aplicação

--- valida.php
+++ valida.php
@@ -1,5 +1,21 @@
-<?php 
+<?php
 
-	echo "acessar";
+include_once ('conexao.php');
+
+
+$email = filter_input(INPUT_POST, 'userEmail', FILTER_SANITIZE_STRING);
+echo $email;
+
+//Verificando se o email ja existe no banco de dados.
+$result_usuario     = "SELECT * FROM usuarios WHERE email='$email' LIMIT 1";
+$resultado_usuario  = mysqli_query($conn, $result_usuario); 
+
+//Essas condições verficam se há alguma dado.
+if (($resultado_usuario) AND($resultado_usuario->num_rows != 0)) {
+    echo 'Acessar!';
+}else{
+    echo 'Erro!';
+}
+
 
 ?>
